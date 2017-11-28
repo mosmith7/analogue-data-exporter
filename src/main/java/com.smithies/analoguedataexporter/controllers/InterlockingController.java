ec15d144dd7c1b22f49de44f18d9babfb79cc873
Mark <msmithies@Marks-MacBook-Pro.local>
2017-11-28 10:14:58 +0000

    typeahead used for site selection and search. Gets site names and then queries again to get the id and fill in the channel select box. does not work if site name has a / in it

--- src/main/java/com.smithies/analoguedataexporter/controllers/InterlockingController.java
+++ src/main/java/com.smithies/analoguedataexporter/controllers/InterlockingController.java
@@ -7,10 +7,13 @@ import com.smithies.analoguedataexporter.valueobjects.InterlockingVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("sites")
@@ -20,9 +23,22 @@ public class InterlockingController {
     private InterlockingRepository repo;
 
     @GetMapping()
-    public List<InterlockingVO> findAll() {
+    public @ResponseBody List<InterlockingVO> findAll() {
         List<Interlocking> sites = new ArrayList<>();
         repo.findAll().forEach(sites::add);
         return InterlockingVOFactory.generateVO(sites);
+    }
+
+    @GetMapping("/names")
+    public @ResponseBody List<String> findAllNames() {
+        List<Interlocking> sites = new ArrayList<>();
+        repo.findAll().forEach(sites::add);
+        List<InterlockingVO> vos = InterlockingVOFactory.generateVO(sites);
+        return vos.stream().map(vo -> vo.getName()).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{name}/id")
+    public @ResponseBody Short getIdForName(@PathVariable("name") String siteName) {
+        return repo.findInterlockingByNameEquals(siteName).getId();
     }
 }
--- src/main/java/com.smithies/analoguedataexporter/repositories/InterlockingRepository.java
+++ src/main/java/com.smithies/analoguedataexporter/repositories/InterlockingRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InterlockingRepository extends CrudRepository<Interlocking, Short> {
+
+    Interlocking findInterlockingByNameEquals(String name);
 }
